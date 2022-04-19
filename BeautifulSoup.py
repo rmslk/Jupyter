bab8b6574973d467cc292f3962e1a803b9fde2d6
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-19 22:00:07 +0300

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -12,19 +12,19 @@ lst = soup.find_all('name')
 for item in lst:
     print(item)
 
-url = "https://matplotlib.org/stable/gallery/index.html"
+url = "https://seaborn.pydata.org/examples/index.html"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 print(soup)
 
-lst = soup.find_all('h2')
+lst = soup.find_all('<h2')
 
 for item in lst:
     print(item)
 
 def clean_item(my_item):
-    position = my_item.find('<a')
+    position = my_item.find('<p>')
     return my_item[4:position]
     
 print("")
@@ -32,28 +32,20 @@ print("")
 for item in lst:
     print(clean_item(str(item)))
 
-lst = soup.find_all(class_ = 'std std-ref')
-
-for item in lst:
-    print(item)
-    
-def clean_item2(my_item):
-    position = my_item.find('</')
-    return my_item[26:position]
-    
-print("")
-
-for item in lst:
-    print(clean_item2(str(item)))
-
-# ~ def clean_item(my_item):
-    # ~ position = my_item.find('<a')
-    # ~ return my_item[4:position]
-
-# ~ print(clean_item('<h2>Text, labels and annotations<a clas'))
+# ~ lst = soup.find_all(class_ = 'std std-ref')
 
 # ~ for item in lst:
-    # ~ print(clean_item(str(item)))
+    # ~ print(item)
+    
+# ~ def clean_item2(my_item):
+    # ~ position = my_item.find('</')
+    # ~ return my_item[26:position]
+    
+# ~ print("")
+
+# ~ for item in lst:
+    # ~ print(clean_item2(str(item)))
+
 
 
 
